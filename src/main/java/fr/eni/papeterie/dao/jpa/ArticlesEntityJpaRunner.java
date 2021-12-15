6f316b66c6ef9c0b7e6e55728c9af379724dc608
31010-67-02 <31010-67-02@exchange.ad.afpanet>
2021-12-15 16:42:18 +0100

    logback not work

--- src/main/java/fr/eni/papeterie/dao/jpa/ArticlesEntityJpaRunner.java
+++ src/main/java/fr/eni/papeterie/dao/jpa/ArticlesEntityJpaRunner.java
@@ -1,12 +1,15 @@
 package fr.eni.papeterie.dao.jpa;
 
 import fr.eni.papeterie.entites.ArticlesEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class ArticlesEntityJpaRunner {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger("ArticlesEntityJpaRunner");
     public static void main(String[] args) {
+
         ArticlesDaoJpa articlesDaoJpa = new ArticlesDaoJpa();
 
         articlesDaoJpa.read().forEach(System.err::println);
@@ -15,7 +18,7 @@ public class ArticlesEntityJpaRunner {
        List<ArticlesEntity> a =  articlesDaoJpa.selectByMotCle("BB");
 
         a.forEach(System.out::println);
-        System.out.println(a.size());
+        LOGGER.debug("Hey");
         a = articlesDaoJpa.SelectByMarque("BBOrange");
         System.out.println(a.size());
         a.forEach(System.out::println);
